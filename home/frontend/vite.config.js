2f2516f526e93e83fc51a855449083e6435b4ae2
Esteban_Alfaro <jealfarosa@unitru.edu.pe>
2026-05-20 03:10:35 -0300

    Add login page desing with two colum layout

--- home/frontend/vite.config.js
+++ home/frontend/vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
   server: {
-    host: '0.0.0.0',
+    host: true,
     port: 3000,
-    watch: {
-      usePolling: true,
-    },
-  },
+  }
 })
