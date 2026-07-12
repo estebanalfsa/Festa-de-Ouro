06c5e088a2307987f3f7ee6d1efa8f26a1192010
FabricioBenitesR <fabricio2006216@gmail.com>
2026-07-12 19:26:38 -0300

    Resolve conflito de merge no App.jsx

--- home/frontend/src/App.jsx
+++ home/frontend/src/App.jsx
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Home from './pages/Home'
 import User from './pages/User'
 import Senha from './pages/Senha'
-=======
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Register from "./pages/Register";
-import Login from "./pages/Login";
->>>>>>> feature/auth
 
 function App() {
   return (
@@ -19,15 +12,12 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-<<<<<<< HEAD
         <Route path="/home" element={<Home />} />
         <Route path='/user' element={<User />} />
         <Route path='/senha' element={<Senha />} />
-=======
->>>>>>> feature/auth
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App;
+export default App
